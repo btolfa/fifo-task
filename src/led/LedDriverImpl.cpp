665050f36ef756f7b4e438dca7065b8f454d08fe
Tengiz Sharafiev <btolfa@gmail.com>
2016-03-19 21:09:05 +0300

    feat: Реализация Led на сообщения логгера
    set действия приводят к выводу сообщения
    get действия проходят тихо.

--- src/led/LedDriverImpl.cpp
+++ src/led/LedDriverImpl.cpp
@@ -6,3 +6,44 @@
  */
 
 #include "LedDriverImpl.hpp"
+
+namespace fifoserver {
+
+void LedDriverImpl::set_color(const LedColor color) {
+    logger_->info("Set color to {}", color);
+    color_ = color;
+}
+
+LedColor LedDriverImpl::get_color() const {
+    return color_;
+}
+
+void LedDriverImpl::set_rate(const uint32_t rate_hz) {
+    if (rate_hz > 5) {
+        logger_->error("Invalid LED blink rate: {}", rate_hz);
+    } else {
+        logger_->info("Set LED blink rate to {}", rate_hz);
+    }
+
+}
+
+uint32_t LedDriverImpl::get_rate() const {
+    return rate_;
+}
+
+void LedDriverImpl::set_state(const LedState state) {
+    if (state == LedState::On) {
+        logger_->info("Enable LED");
+    } else {
+        logger_->info("Disable LED");
+    }
+
+    b_enabled_ = (state == LedState::On);
+}
+
+bool LedDriverImpl::is_enabled() const {
+    return b_enabled_;
+}
+
+
+}
