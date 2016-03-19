2162be6b06f377945a88df50024fd5847783ee82
Tengiz Sharafiev <btolfa@gmail.com>
2016-03-19 21:59:35 +0300

    feat: Примитивный конструктор для Server
    Конструктор ничего не делает, а только сохраняет
    свои параметры

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -6,3 +6,13 @@
  */
 
 #include "Server.hpp"
+#include "../led/LedDriver.hpp"
+
+namespace fifoserver {
+
+Server::Server(fs::path const &fifopath, asio::io_service &io_service, LedDriver & led)
+    : fifopath_{fifopath}, io_service_{io_service}, led_{led} {
+
+}
+
+}
