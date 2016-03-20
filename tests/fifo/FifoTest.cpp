48521c4df395b2103bfc629589663b83e852650b
Tengiz Sharafiev <btolfa@gmail.com>
2016-03-20 10:19:56 +0300

    test: FifoTest который ничего не делает

---
+++ tests/fifo/FifoTest.cpp
@@ -0,0 +1,20 @@
+/**
+ * @author Tengiz Sharafiev <btolfa@gmail.com>
+ * @date 20.03.16.
+ */
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+namespace {
+
+class FifoTest : public ::testing::Test {
+protected:
+};
+
+TEST_F(FifoTest, Should) {
+    FAIL() << "Not implemented";
+}
+
+
+}
