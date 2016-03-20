dec3f227f3d8ba7af12b9474182f2b793a635e78
Tengiz Sharafiev <btolfa@gmail.com>
2016-03-20 20:28:00 +0300

    feat: Реализация сервера принимающего по fifo строку
    Он должен вызывать конструктор, который передаёт работу SessionFactory

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -1,18 +1,68 @@
 /**
- *
- * @author Tengiz Sharafiev<btolfa@gmail.com>
- * @date 19.03.16
- * 
+ * @author Tengiz Sharafiev <btolfa@gmail.com>
+ * @date 20.03.16.
  */
 
 #include "Server.hpp"
-#include "../led/LedDriver.hpp"
+
+#include <boost/filesystem/fstream.hpp>
+#include <linux/limits.h>
+#include <sstream>
+
+namespace fs = boost::filesystem;
 
 namespace fifoserver {
 
-Server::Server(fs::path const &fifopath)
-    : fifopath_{fifopath} {
+Server::Server(boost::filesystem::path const &fifopath, fifoserver::SessionFactory &factory)
+        : fifopath_{fifopath}, factory_{factory}
+{
+    // Пытаемся создать логгер, если уже существует, то берём из готовый
+    try {
+        lg_ = spdlog::stdout_logger_mt("server");
+    } catch (spdlog::spdlog_ex const& exception) {
+        lg_ = spdlog::get("server");
+    }
 
+}
+
+void Server::run() {
+    bool is_quit = false;
+    while (! is_quit) {
+        // Блокируется, до тех пор пока не подключились с другой стороны
+        fs::ifstream fifostream{fifopath_};
+
+        for (std::string line; std::getline(fifostream, line, '\n');) {
+            if (line == "quit") {
+                is_quit = true;
+                lg_->info("Received quit request");
+                break;
+            } else {
+                auto parameters = split(line);
+                if (parameters.size() == 2) {
+                    lg_->info("Received connecting with input fifo - {} and output fifo - {}",
+                              parameters[0], parameters[1]);
+                    factory_.create_detached(parameters[0], parameters[1]);
+                } else {
+                    // Если параметров больше 2х, ничего не делаем
+                    // Т.к. такое поведение не определено
+                    lg_->warn("Received request with more then 2 parameters, ignoring: {}", line);
+                }
+            }
+        }
+    }
+}
+
+// TODO: Реальные пути могут содержать пробелы, нужно парсить пришедшие строки
+// TODO: учитываю такую возможность. Наверняка есть готовая читалка строки из путей
+std::vector<std::string> Server::split(const std::string &line) {
+    using namespace std;
+    istringstream iss{line};
+
+    vector<string> results;
+    results.reserve(2);
+
+    copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(results));
+    return results;
 }
 
 }
--- src/server/Server.hpp
+++ src/server/Server.hpp
@@ -1,23 +1,26 @@
 /**
- *
- * @author Tengiz Sharafiev<btolfa@gmail.com>
- * @date 19.03.16
- * 
+ * @author Tengiz Sharafiev <btolfa@gmail.com>
+ * @date 20.03.16.
  */
 
 #pragma once
 
-#include <boost/filesystem.hpp>
-
-namespace fs = boost::filesystem;
+#include "../session/SessionFactory.hpp"
+#include <spdlog/spdlog.h>
 
 namespace fifoserver {
 
 class Server {
 public:
-    Server(fs::path const& fifopath);
+    Server(boost::filesystem::path const& fifopath, SessionFactory& factory);
+
+    void run();
+    static std::vector<std::string> split(const std::string &line);
 private:
-    fs::path fifopath_;
+    boost::filesystem::path fifopath_;
+    SessionFactory & factory_;
+
+    std::shared_ptr<spdlog::logger> lg_;
 };
 
 }
