10649b2c00cd102e4237168eed97c8e5b3fe2c1d
Tengiz Sharafiev <btolfa@gmail.com>
2016-03-20 19:03:58 +0300

    feat: Пустой класс для Session
    Чтобы было что собирать для цели session

---
+++ src/session/Session.cpp
@@ -0,0 +1,8 @@
+/**
+ *
+ * @author Tengiz Sharafiev<btolfa@gmail.com>
+ * @date 20.03.16
+ * 
+ */
+
+#include "Session.hpp"
---
+++ src/session/Session.hpp
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author Tengiz Sharafiev<btolfa@gmail.com>
+ * @date 20.03.16
+ * 
+ */
+
+#pragma once
+
+namespace fifoserver {
+
+class Session {
+public:
+private:
+};
+
+}
+
+
