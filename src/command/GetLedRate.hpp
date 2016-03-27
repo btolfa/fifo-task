6c1090ec31ca6d58bee26d4ad073c08c000657b4
Tengiz Sharafiev <btolfa@gmail.com>
2016-03-27 12:25:27 +0300

    feat: Пустые реализации комманд

---
+++ src/command/GetLedRate.hpp
@@ -0,0 +1,25 @@
+/**
+ *
+ * @author Tengiz Sharafiev<btolfa@gmail.com>
+ * @date 27.03.16
+ * 
+ */
+
+#pragma once
+
+#include "Command.hpp"
+
+namespace fifoserver {
+namespace command {
+
+class GetLedRate : public Command{
+public:
+    virtual std::string execute(LedDriver &ledDriver) override final {
+        return {};
+    }
+};
+
+}
+}
+
+
